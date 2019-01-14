f0abf747c8d1adb75ffe6877be13a2820dffb743
Corey <1500812@abertay.ac.uk>
2019-01-14 13:21:52 +0000

    Update cloneS3.sh
    Made more accessable with open arguments for bucket names, untested

--- cloneS3.sh
+++ cloneS3.sh
@@ -1,8 +1,8 @@
-# Copies the content of an S3 box
+# Copies the content of an S3 box from Argument $1
 cd ~
 mkdir s3
-sudo s3fs forensics-dd -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 s3
-mkdir diskImages
-sudo cp s3/* diskImages/
+sudo s3fs $1 -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 s3
+mkdir "s3_contents_$1"
+sudo cp s3/* "s3_contents_$1/"
 sudo fusermount -u s3
 rm -r s3/
